abab11829a8b682211c21a588fde8be6fd459a48
mullinslp <113126794+mullinslp@users.noreply.github.com>
2022-11-17 15:00:13 -0800

    Delete meanvsmean directory

--- meanvsmean/tests/testthat/test-mean_vs_mean.R
+++
@@ -1,5 +0,0 @@
-test_that("mean_vs_mean works", {
-  expect_equal(mean_vs_mean(c(2, 4, 6), c(1, 2, 3)), 2)
-  expect_equal(mean_vs_mean(c(5, 10, 15), c(5, NA, 15)), 0)
-  expect_error(mean_vs_mean(vancouver_trees$diameter), "Missing 'y' variable. Double check your input!")
-})
